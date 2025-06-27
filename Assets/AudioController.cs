1425169499ca468ce16552cfeea8f86ff51374f0
pinponkun <hayashidareki0423@gmail.com>
2025-06-27 15:59:05 +0900

    success_BGM_adjust/failire_SE_adjust

---
+++ Assets/AudioController.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+using UnityEngine.UIElements.Experimental;
+
+public class AudioController : MonoBehaviour
+{
+    //Audioミキサーを入れるとこ
+    [SerializeField] AudioMixer audioMixer;
+
+    //それぞれのスライダーを入れるとこ
+    [SerializeField] Slider BGMSlider;
+    [SerializeField] Slider SESlider;
+
+    private void Start()
+    {
+        if (BGMSlider != null)
+        {
+            BGMSlider.onValueChanged.AddListener((value) =>
+            {
+                value = Mathf.Clamp01(value);
+
+                float decibel = 20f * Mathf.Log10(value);
+                decibel = Mathf.Clamp(decibel, -80f, 0f);
+                audioMixer.SetFloat("BGM", decibel);
+            });
+        }
+
+        if (SESlider != null)
+        {
+            SESlider.onValueChanged.AddListener((value) =>
+            {
+                value = Mathf.Clamp01(value);
+
+                float decibel = 20f * Mathf.Log10(value);
+                decibel = Mathf.Clamp(decibel, -80f, 0f);
+                audioMixer.SetFloat("SE", decibel);
+            });
+        }
+    }
+}
